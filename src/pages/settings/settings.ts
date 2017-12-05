52105c3895ad4f0aaf14ed103ab6fe8ffebc3c68
Jonas Schmitt <jonas.schmitt@me.com>
2017-12-05 16:15:12 +0100

    add: navigation structure to AdminPage and SettingsPage

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core'
 import { IonicPage, NavController, NavParams, Events } from 'ionic-angular'
 
+// provider imports
+import { PlatformServiceProvider } from '../../providers/platform-service/platform-service'
+
 @IonicPage()
 @Component({
     selector: 'page-settings',
     templateUrl: 'settings.html'
 })
 export class SettingsPage {
+    plt: string
+
     constructor(
         public navCtrl: NavController,
         public navParams: NavParams,
-        public events: Events
-    ) {}
+        public events: Events,
+        public platform: PlatformServiceProvider
+    ) {
+        this.plt = this.platform.getPlatform()
+    }
 
     // function which publishes events
     publishEvent(eventName: string) {
