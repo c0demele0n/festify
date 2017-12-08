98f02ecf8e3a25315a2327262f26c3d78bce4cc5
Sascha Keuchel <inf2928@hs-worms.de>
2017-12-08 09:16:50 +0100

    added rows for playlist

--- src/pages/admin/admin.ts
+++ src/pages/admin/admin.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from '@angular/core'
+import { IonicPage, NavController, NavParams } from 'ionic-angular'
 
 /**
  * Generated class for the AdminPage page.
@@ -10,16 +10,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-admin',
-  templateUrl: 'admin.html',
+    selector: 'page-admin',
+    templateUrl: 'admin.html'
 })
 export class AdminPage {
+    playlists = []
+    generalName: string = 'Name of Party:'
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AdminPage');
-  }
-
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.playlists = [
+            this.playlist1,
+            this.playlist2,
+            this.playlist1,
+            this.playlist2
+        ]
+    }
+    playlist1: any = {
+        Name: 'Meine Plylist1',
+        AnzTitel: '43',
+        Titel: {
+            Name: 'Schlecht',
+            Dauer: '5.00'
+        }
+    }
+    playlist2: any = {
+        Name: 'Meine Plylist2',
+        AnzTitel: '5',
+        Titel: {
+            Name: 'Toll',
+            Dauer: '7.00'
+        }
+    }
 }
