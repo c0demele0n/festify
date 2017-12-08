b9be8bad7590138d37c1c440908b1cd2287aaf6e
Gueven Sezis <inf2865@hs-worms.de>
2017-12-08 11:39:27 +0100

    change: rootPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,16 @@ import { Component } from '@angular/core'
 import { Platform } from 'ionic-angular'
 import { StatusBar } from '@ionic-native/status-bar'
 import { SplashScreen } from '@ionic-native/splash-screen'
-<<<<<<< HEAD
 
 // page imports
 import { HomePage } from '../pages/home/home'
 import { NavPage } from '../pages/nav/nav'
 
-=======
-
-import { HomePage } from '../pages/home/home'
-import { AdminPage } from '../pages/admin/admin'
->>>>>>> 1b4123ba69ead789cf28fafb3040c3e16e68b16c
 @Component({
     templateUrl: 'app.html'
 })
 export class MyApp {
-<<<<<<< HEAD
     rootPage: any = HomePage
-=======
-    rootPage: any = AdminPage
->>>>>>> 1b4123ba69ead789cf28fafb3040c3e16e68b16c
 
     constructor(
         platform: Platform,
