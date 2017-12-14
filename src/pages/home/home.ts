ba77d0d9da1ecc1b9dc87495db6b008f0a04433d
Jonas Schmitt <jonas.schmitt@me.com>
2017-12-14 14:14:16 +0100

    add: basic navigation for enter and exit the party

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,35 +1,57 @@
 import { Component } from '@angular/core'
-import { NavController, Nav } from 'ionic-angular'
+import { NavController, Nav, AlertController } from 'ionic-angular'
 
 // page imports
 import { NavPage } from '../nav/nav'
 
 @Component({
-    selector: 'page-home',
-    templateUrl: 'home.html'
+  selector: 'page-home',
+  templateUrl: 'home.html'
 })
 export class HomePage {
-    constructor(public navCtrl: NavController, public nav: Nav) {}
-    createParty() {
-        /*
-      TODO:       Implement complete logic
-      Dependency: Task MPJ-19
-    */
+  constructor(
+    public navCtrl: NavController,
+    public nav: Nav,
+    public alertCtrl: AlertController
+  ) {}
 
-        // redirect to NavPage
-        this.nav.setRoot(NavPage)
-    }
-    joinParty() {
-        /*
+  createParty() {
+    let alert = this.alertCtrl.create({
+      title: 'Set Partyname',
+      inputs: [
+        {
+          name: 'partyName',
+          placeholder: 'Partyname'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {}
+        },
+        {
+          text: 'Party hard',
+          handler: data => {
+            // redirect to NavPage
+            this.nav.setRoot(NavPage, { partyName: data.partyName })
+          }
+        }
+      ]
+    })
+    alert.present()
+  }
+  joinParty() {
+    /*
       TODO:       Implement complete logic
       Dependency: After finishing the logic for the host
     */
-    }
-    reopenParty() {
-        /*
+  }
+  reopenParty() {
+    /*
       TODO:       Implement complete logic
       Dependency: Task MPJ-19
                   Logic for closing the Party (Settings)
     */
-    }
+  }
 }
--- src/pages/nav/nav.ts
+++ src/pages/nav/nav.ts
@@ -1,15 +1,16 @@
 import { Component, ViewChild } from '@angular/core'
 import {
-    IonicPage,
-    Nav,
-    NavController,
-    NavParams,
-    Events,
-    Platform,
-    PopoverController,
-    ViewController,
-    ModalController,
-    Tabs
+  IonicPage,
+  Nav,
+  NavController,
+  NavParams,
+  Events,
+  Platform,
+  PopoverController,
+  ViewController,
+  ModalController,
+  Tabs,
+  AlertController
 } from 'ionic-angular'
 
 // page imports
@@ -25,155 +26,178 @@ import { PlatformServiceProvider } from '../../providers/platform-service/platfo
 
 @IonicPage()
 @Component({
-    selector: 'page-nav',
-    templateUrl: 'nav.html'
+  selector: 'page-nav',
+  templateUrl: 'nav.html'
 })
 export class NavPage {
-    // local tab references to the tabs
-    @ViewChild('tabsIos') tabRefIos: Tabs
-    @ViewChild('tabsAndroid') tabRefAndroid: Tabs
+  // local tab references to the tabs
+  @ViewChild('tabsIos') tabRefIos: Tabs
+  @ViewChild('tabsAndroid') tabRefAndroid: Tabs
 
-    // selected tab index when page is loaded (standart tab)
-    selectedTabIndex: number = 0
+  // selected tab index when page is loaded (standart tab)
+  selectedTabIndex: number = 0
 
-    // current tab name
-    currentTabName: string = 'QueuePage'
+  // current tab name
+  currentTabName: string = 'QueuePage'
 
-    root = QueuePage
+  root = QueuePage
 
-    tab1: any = {
-        Name: 'Queue',
-        Page: QueuePage,
-        Icon: 'icon-playlist'
+  tab1: any = {
+    Name: 'Queue',
+    Page: QueuePage,
+    Icon: 'icon-playlist'
+  }
+  tab2: any = {
+    Name: 'Admin',
+    Page: AdminPage,
+    Icon: 'contact'
+  }
+  tab3: any = {
+    Name: 'Settings',
+    Page: SettingsPage,
+    Icon: 'settings'
+  }
+  tab4: any = {
+    Name: 'TV Mode',
+    Page: TvModePage,
+    Icon: 'desktop'
+  }
+
+  // array which holds all tabs
+  tabs = [this.tab1, this.tab2, this.tab3, this.tab4]
+
+  // string which holds the current platform name
+  plt: string
+
+  constructor(
+    public nav: Nav,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public events: Events,
+    public platform: PlatformServiceProvider,
+    public popoverCtrl: PopoverController,
+    public modalCtrl: ModalController,
+    public alertCtrl: AlertController
+  ) {
+    // get partyname from nav params
+    console.log('Partyname: ' + this.navParams.get('partyName'))
+
+    // get current platform
+    this.plt = this.platform.getPlatform()
+    if (this.plt == 'desktopweb') {
+      this.openPage(this.tab1.Page, this.tab1.Name)
     }
-    tab2: any = {
-        Name: 'Admin',
-        Page: AdminPage,
-        Icon: 'contact'
-    }
-    tab3: any = {
-        Name: 'Settings',
-        Page: SettingsPage,
-        Icon: 'settings'
-    }
-    tab4: any = {
-        Name: 'TV Mode',
-        Page: TvModePage,
-        Icon: 'desktop'
-    }
 
-    // array which holds all tabs
-    tabs = [this.tab1, this.tab2, this.tab3, this.tab4]
+    // subscribe to all events
+    this.events.subscribe('all-events', eventName => {
+      if (eventName == 'exit-party') {
+        this.exitParty()
+      }
+      if (eventName == 'tv-mode') {
+        this.showTvMode()
+      }
+      if (eventName == 'share-party') {
+        this.shareParty()
+      }
+      if (eventName == 'show-settings') {
+        this.showSettings()
+      }
+      if (eventName == 'toggle-more') {
+        this.toggleMore()
+      }
+    })
+  }
 
-    // string which holds the current platform name
-    plt: string
-
-    constructor(
-        public nav: Nav,
-        public navCtrl: NavController,
-        public navParams: NavParams,
-        public events: Events,
-        public platform: PlatformServiceProvider,
-        public popoverCtrl: PopoverController,
-        public modalCtrl: ModalController
-    ) {
-        // get current platform
-        this.plt = this.platform.getPlatform()
-        if (this.plt == 'desktopweb') {
-            this.openPage(this.tab1.Page, this.tab1.Name)
+  // function which shutdowns the current party
+  exitParty() {
+    console.log('exitParty()')
+    let alert = this.alertCtrl.create({
+      title: 'Confirm',
+      message: 'Do you really want to exit the party?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {}
+        },
+        {
+          text: 'Exit',
+          handler: () => {
+            // navigate to HomePage (StartPage)
+            this.nav.setRoot(HomePage)
+          }
         }
+      ]
+    })
+    alert.present()
+  }
 
-        // subscribe to all events
-        this.events.subscribe('all-events', eventName => {
-            if (eventName == 'exit-party') {
-                this.exitParty()
-            }
-            if (eventName == 'tv-mode') {
-                this.showTvMode()
-            }
-            if (eventName == 'share-party') {
-                this.shareParty()
-            }
-            if (eventName == 'show-settings') {
-                this.showSettings()
-            }
-            if (eventName == 'toggle-more') {
-                this.toggleMore()
-            }
-        })
+  // function which switches to the tv mode
+  showTvMode() {
+    console.log('tvMode()')
+
+    let tvModeModal = this.modalCtrl.create(
+      TvModePage,
+      {},
+      {
+        showBackdrop: true,
+        enableBackdropDismiss: true
+      }
+    )
+    tvModeModal.present()
+  }
+
+  // function which provides a share link to the current party
+  shareParty() {
+    console.log('shareParty()')
+  }
+
+  // function which pushes the settings-page to the navigation stack
+  showSettings() {
+    console.log('showSettings()')
+
+    let settingsModal = this.modalCtrl.create(
+      SettingsPage,
+      {},
+      {
+        showBackdrop: true,
+        enableBackdropDismiss: true
+      }
+    )
+    settingsModal.present()
+  }
+
+  // function which toggles the 'more' menu on android devices
+  toggleMore() {
+    console.log('toggleMore()')
+    let morePopover = this.popoverCtrl.create(
+      MorePage,
+      {},
+      { cssClass: 'more-popover' }
+    )
+    morePopover.present()
+  }
+
+  // function which pushes a new page to the navigation stack (only for web-view)
+  openPage(page: any, pageName?: string) {
+    this.root = page
+    this.namePage(pageName)
+  }
+
+  // function which listens to the ionChange tab event
+  tabChanged(pltName: string) {
+    if (this.tabRefIos != null || this.tabRefAndroid != null) {
+      if (pltName == 'ios') {
+        this.namePage(this.tabRefIos.getSelected().tabTitle)
+      }
+      if (pltName == 'android') {
+        this.namePage(this.tabRefAndroid.getSelected().tabTitle)
+      }
     }
+  }
 
-    // function which shutdowns the current party
-    exitParty() {
-        console.log('exitParty()')
-    }
-
-    // function which switches to the tv mode
-    showTvMode() {
-        console.log('tvMode()')
-
-        let tvModeModal = this.modalCtrl.create(
-            TvModePage,
-            {},
-            {
-                showBackdrop: true,
-                enableBackdropDismiss: true
-            }
-        )
-        tvModeModal.present()
-    }
-
-    // function which provides a share link to the current party
-    shareParty() {
-        console.log('shareParty()')
-    }
-
-    // function which pushes the settings-page to the navigation stack
-    showSettings() {
-        console.log('showSettings()')
-
-        let settingsModal = this.modalCtrl.create(
-            SettingsPage,
-            {},
-            {
-                showBackdrop: true,
-                enableBackdropDismiss: true
-            }
-        )
-        settingsModal.present()
-    }
-
-    // function which toggles the 'more' menu on android devices
-    toggleMore() {
-        console.log('toggleMore()')
-        let morePopover = this.popoverCtrl.create(
-            MorePage,
-            {},
-            { cssClass: 'more-popover' }
-        )
-        morePopover.present()
-    }
-
-    // function which pushes a new page to the navigation stack (only for web-view)
-    openPage(page: any, pageName?: string) {
-        this.root = page
-        this.namePage(pageName)
-    }
-
-    // function which listens to the ionChange tab event
-    tabChanged(pltName: string) {
-        if (this.tabRefIos != null || this.tabRefAndroid != null) {
-            if (pltName == 'ios') {
-                this.namePage(this.tabRefIos.getSelected().tabTitle)
-            }
-            if (pltName == 'android') {
-                this.namePage(this.tabRefAndroid.getSelected().tabTitle)
-            }
-        }
-    }
-
-    // function which names the current page
-    namePage(pageName: string) {
-        this.currentTabName = pageName
-    }
+  // function which names the current page
+  namePage(pageName: string) {
+    this.currentTabName = pageName
+  }
 }
