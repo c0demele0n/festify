07f8c69b1f06ab08c44498cb90dbe6a655a7e169
Dennis Kniel <inf2060@hs-worms.de>
2017-12-14 13:52:50 +0100

    change: paths to relative and absolute

--- src/pages/admin/admin.ts
+++ src/pages/admin/admin.ts
@@ -6,33 +6,33 @@ import { PlatformServiceProvider } from '../../providers/platform-service/platfo
 
 @IonicPage()
 @Component({
-    selector: 'page-admin',
-    templateUrl: 'admin.html'
+  selector: 'page-admin',
+  templateUrl: 'admin.html'
 })
 export class AdminPage {
-    plt: string
-    playlists = []
-    generalName: string = 'Name of Party:'
-    amount: String = 'Nr. of Titles:'
+  plt: string
+  playlists = []
+  generalName: string = 'Name of Party:'
+  amount: String = 'Nr. of Titles:'
 
-    constructor(
-        public navCtrl: NavController,
-        public navParams: NavParams,
-        public events: Events,
-        public platform: PlatformServiceProvider
-    ) {
-        // get current platform
-        this.plt = this.platform.getPlatform()
-        this.playlists = [
-            this.playlist1,
-            this.playlist1,
-            this.playlist1,
-            this.playlist1
-        ]
-    }
-    playlist1: any = {
-        Name: 'Cool Playlist !',
-        AnzTitel: '43',
-        thumbnail: '../assets/imgs/logo.png'
-    }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public events: Events,
+    public platform: PlatformServiceProvider
+  ) {
+    // get current platform
+    this.plt = this.platform.getPlatform()
+    this.playlists = [
+      this.playlist1,
+      this.playlist1,
+      this.playlist1,
+      this.playlist1
+    ]
+  }
+  playlist1: any = {
+    Name: 'Cool Playlist !',
+    AnzTitel: '43',
+    thumbnail: 'assets/imgs/logo.png'
+  }
 }
