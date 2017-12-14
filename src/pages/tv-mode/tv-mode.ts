e950b0044e6ebc603858b8232b7eab46b42121df
Dennis Kniel <inf2060@hs-worms.de>
2017-12-14 15:28:28 +0100

    change: started tv-mode ui

--- src/pages/tv-mode/tv-mode.ts
+++ src/pages/tv-mode/tv-mode.ts
@@ -6,19 +6,36 @@ import { PlatformServiceProvider } from '../../providers/platform-service/platfo
 
 @IonicPage()
 @Component({
-    selector: 'page-tv-mode',
-    templateUrl: 'tv-mode.html'
+  selector: 'page-tv-mode',
+  templateUrl: 'tv-mode.html'
 })
 export class TvModePage {
-    plt: string
+  plt: string
+  queue = []
+  meta = []
 
-    constructor(
-        public navCtrl: NavController,
-        public navParams: NavParams,
-        public events: Events,
-        public platform: PlatformServiceProvider
-    ) {
-        // get current platform
-        this.plt = this.platform.getPlatform()
-    }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public events: Events,
+    public platform: PlatformServiceProvider
+  ) {
+    // get current platform
+    this.plt = this.platform.getPlatform()
+    ;(this.queue = [this.track1, this.track1, this.track1]),
+      (this.meta = [this.url, this.id])
+  }
+
+  track1: any = {
+    name: 'Track X',
+    artist: 'Artist X',
+    thumbnail: '../assets/imgs/logo.png'
+  }
+  current: any = {
+    name: 'In the end',
+    artist: 'Linkin Park',
+    thumbnail: '../assets/imgs/logo.png'
+  }
+  url: string = 'festify.us'
+  id: number = 12345
 }
