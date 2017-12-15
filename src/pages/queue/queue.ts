1cf07d463c16a09205c877354d4be8af53ea441d
Jonas Schmitt <jonas.schmitt@me.com>
2017-12-15 13:55:48 +0100

    add: search function to QueuePage and show the results in a list

--- src/pages/queue/queue.ts
+++ src/pages/queue/queue.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, Events } from 'ionic-angular'
 
 // provider imports
 import { PlatformServiceProvider } from '../../providers/platform-service/platform-service'
+import { SpotifyProvider } from '../../providers/spotify/spotify'
 
 @IonicPage()
 @Component({
@@ -10,23 +11,47 @@ import { PlatformServiceProvider } from '../../providers/platform-service/platfo
   templateUrl: 'queue.html'
 })
 export class QueuePage {
+  searchString: string
+  tracks: any
   plt: string
+  Code: number = 504215
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public events: Events,
-    public platform: PlatformServiceProvider
+    public platform: PlatformServiceProvider,
+    public spotify: SpotifyProvider
   ) {
     // get current platform
     this.plt = this.platform.getPlatform()
   }
 
-  Code: any = 504215
-
   // function which publishes events
   publishEvent(eventName: string) {
     // this.viewCtrl.dismiss()
     this.events.publish('all-events', eventName)
+  }
+
+  // function which calls the spotify provider to search for tracks
+  async getTracks(ev: any) {
+    // <only for dev>
+    if (this.spotify.isLoggedIn()) {
+      // redirect to NavPage
+    } else {
+      this.spotify.login()
+    }
+    // </only for dev>
+
+    console.log(this.searchString)
+
+    console.log('search for: ' + ev.target.value)
+    this.tracks = await this.spotify.getTracks(ev.target.value)
+    console.log(this.tracks)
+
+    if (this.searchString == '') {
+      console.log('empty')
+      this.tracks = {}
+    }
   }
 }
